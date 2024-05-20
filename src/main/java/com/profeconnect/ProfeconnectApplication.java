2effa6191619c7385c3ac12e88eded7d41f4ff2a
Felipe <marconizero@gmail.com>
2024-05-20 18:41:38 -0300

    feat: add initial files for spring app

---
+++ src/main/java/com/profeconnect/ProfeconnectApplication.java
@@ -0,0 +1,13 @@
+package com.profeconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfeconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfeconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/profeconnect/ProfeconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profeconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfeconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
