c2126fe9b69417c2ca0dc65c2528296da8cc4083
Felipe <marconizero@gmail.com>
2024-05-24 18:39:06 -0300

    feat: parse studentDTO to User

--- src/main/java/com/profeconnect/controller/StudentSignupController.java
+++ src/main/java/com/profeconnect/controller/StudentSignupController.java
@@ -6,16 +6,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.profeconnect.domain.entity.User;
 import com.profeconnect.dto.StudentDTO;
+import com.profeconnect.service.StudentService;
 
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/app/studentsignup")
 public class StudentSignupController {
+    private final StudentService studentService;
+    public StudentSignupController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @PostMapping
     public ResponseEntity<String> signup(@RequestBody @Valid StudentDTO studentDTO) {
+        User student = studentService.fromDTO(studentDTO);
         return ResponseEntity.ok("token");
     }
 }
