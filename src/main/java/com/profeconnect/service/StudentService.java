76c079106c25a4cf641031c7c1e3075bdf309ab9
Felipe <marconizero@gmail.com>
2024-05-24 18:40:24 -0300

    feat: create signupStudent method on AuthenticationService

--- src/main/java/com/profeconnect/service/StudentService.java
+++ src/main/java/com/profeconnect/service/StudentService.java
@@ -7,6 +7,11 @@ import com.profeconnect.dto.StudentDTO;
 
 @Service
 public class StudentService {
+    public User signupStudent(User user) {
+        User usuraio = new User();
+        return usuraio;
+    }
+
     public User fromDTO(StudentDTO student) {
         User user = new User();
         return user;
