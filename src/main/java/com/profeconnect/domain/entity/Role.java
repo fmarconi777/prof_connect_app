65738457c7665aa603dcab31e47d59e2a766e0e6
Elioenai <elioenai.eas@gmail.com>
2024-05-31 01:51:24 -0300

    feat: create Role Entity

---
+++ src/main/java/com/profeconnect/domain/entity/Role.java
@@ -0,0 +1,89 @@
+package com.profeconnect.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "role_id", unique = true)
+    private Long roleId;
+
+    private String name;
+
+    public Role() {
+
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((roleId == null) ? 0 : roleId.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        if (roleId == null) {
+            if (other.roleId != null)
+                return false;
+        } else if (!roleId.equals(other.roleId))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    public enum Values {
+        ADMIN(1L), 
+        STUDENT(2L),
+        TEACHER(3L);
+
+        long roleId;
+        Values(long roleId) {
+            this.roleId = roleId;
+        }
+
+        public long getRoleId(){
+            return roleId;
+        }
+    }
+
+}
