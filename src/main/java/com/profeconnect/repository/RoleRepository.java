9690699f3f09ebfdc918057fcc2b49c1641f99f7
Elioenai <elioenai.eas@gmail.com>
2024-05-31 01:55:53 -0300

    feat: create RoleRepository

---
+++ src/main/java/com/profeconnect/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.profeconnect.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.profeconnect.domain.entity.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByRolename(String rolename);
+}
